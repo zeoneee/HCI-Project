4ca024c7b3ddf6c7e7ae52eaf0a1dd0a159ec448
HeeJeongOh <62127798+HeeJeongOh@users.noreply.github.com>
2021-05-14 23:04:08 +0900

    ImageProcess - extract 수정

--- Project1/Project1/ImageProcess.cpp
+++ Project1/Project1/ImageProcess.cpp
@@ -51,23 +51,41 @@ string chooseImage() {
 	return res;
 }
 
+// 흑백과 컬러 구분해서 하려햇으나 우리 눈엔 흑백인 이미지도 rgb채널을 가지고 있음
+// 가지고 있는 이미지들은 전처리로 흑백으로 만들든 해야할 듯
 Mat extract(Mat img) {
+	cout << img.channels() << endl;
 
-	Mat img2gray; // 합성할 이미지를 흑백으로 변환 
-	cvtColor(img, img2gray, CV_BGR2GRAY);
+	Mat img_gray;
+	cvtColor(img, img_gray, COLOR_BGR2GRAY);
+	//imshow("img_gray", img_gray);
 
-	Mat mask;	// 흑백 이미지를 임계값으로 이진화해 마스크 생성
-	threshold(img2gray, mask, 250, 255, THRESH_BINARY);
+	// 컬러 도안
+	if (img.channels() != 1) {
+		Mat mask;	// 흑백 이미지를 임계값으로 이진화해 마스크 생성
+		adaptiveThreshold(img_gray, mask, 255, THRESH_BINARY, ADAPTIVE_THRESH_GAUSSIAN_C, 5, 10);
+		//imshow("mask", mask);
 
-	// 해당 마스크의 역처리
-	Mat mask_inv;
-	bitwise_not(mask, mask_inv);
-	imshow("mask_inv", mask_inv);
+		// 해당 마스크의 역처리
+		Mat mask_inv;
+		bitwise_not(mask, mask_inv);
 
-	// 워터마크 이미지에서 워터마크 영역만 추출(and 비트연산, 역 마스크 영역)
-	Mat img2_fg;
-	bitwise_and(img, img, img2_fg, mask = mask_inv);
-	imshow("img_small", img);
-	
-	return img2_fg;
+		// 워터마크 이미지에서 워터마크 영역만 추출(and 비트연산, 역 마스크 영역)
+		Mat img_result;
+		bitwise_and(img, img, img_result, mask = mask_inv);
+		imshow("img_result", img_result);
+		return img_result;
+
+	}
+	// 흑백 도안
+	else {
+		Mat img_binary;
+		threshold(img_gray, img_binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
+		//imshow("img_binary", img_binary);
+
+		CEdge ce;
+		Mat img_edge = ce.GS_canny_edge_Canny(img_binary, 50, 100);
+		imshow("img_edge", img_edge);
+		return img_edge;
+	}
 }
